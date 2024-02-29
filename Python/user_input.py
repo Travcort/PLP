898e034f09ef942c36bae42e4a2ec78a2c675cb6
Travcort <tarvkoome@gmail.com>
2024-02-29 16:59:22 +0300

    Edited portifolio and added the python file

---
+++ Python/user_input.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live currently? ")
+
+print("Hello " + name + ", you are " + age + " years old and you live in " + location + ".")
+print("Have a nice day!")
