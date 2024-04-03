3b9e5d0e4b737c654aae78374dae8863ebcc92cf
Travcort <tarvkoome@gmail.com>
2024-04-03 14:36:27 +0300

    File Handling Assignment Submission

--- Python/file-handling/file_handling_assignment.py
+++ Python/file-handling/file_handling_assignment.py
@@ -1,5 +1,5 @@
 #Compiling the whole thing as one
-with open("Python/file handling/my_file.txt", "w+") as file:
+with open("Python/file-handling/my_file.txt", "w+") as file:
     #Writing lines to the file
     file.writelines(["Hey there", "\nHow are you?", "\nYou are number 43 in this task", "\nTry your luck next time.", "\nAdios"])
     #Read the contents of the file
@@ -7,7 +7,7 @@ with open("Python/file handling/my_file.txt", "w+") as file:
     #Printing the contents of the file on the console
     print(file_contents)
     #Modifying the file to open in append mode
-    open("Python/file handling/my_file.txt", "a")
+    open("Python/file-handling/my_file.txt", "a")
     #Adding a few more lines to the end
     file.writelines(["\nNow that's better", "\nCongrats for the improvement!!"])
     #Printing the Current contents of the file
